387e97a4cdbadbc545a753697a1015a1e4fc06d4
Wesley Yan Soares Brehmer <102262759+simplyYan@users.noreply.github.com>
2024-01-04 14:49:12 +0000

    Update mac.weepsbox.sh

--- mac.weepsbox.sh
+++ mac.weepsbox.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if command -v python3 &>/dev/null; then
+    echo "Python is installed. Installing packages..."
+
+    python3 -m pip install numba py_mini_racer requests scrapy sqlalchemy beautifulsoup4 scipy scikit-learn
+else
+    echo "Python is not installed, ignoring...."
+fi
+
 if [ $# -eq 0 ]; then
     echo "Using: weepsbox TARGET"
     exit 1
