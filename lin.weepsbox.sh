7dce0f8e9244d6bfa4fcccab8255e31ff5e9acc2
Wesley Yan Soares Brehmer <102262759+simplyYan@users.noreply.github.com>
2024-01-04 14:52:29 +0000

    Update lin.weepsbox.sh

--- lin.weepsbox.sh
+++ lin.weepsbox.sh
@@ -9,6 +9,39 @@ else
     echo "Python is not installed, ignoring...."
 fi
 
+# Função para detectar a distribuição Linux
+get_linux_distribution() {
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        if [ -n "$ID" ]; then
+            echo "$ID"
+            return
+        fi
+    fi
+    echo "unknown"
+}
+
+linux_distribution=$(get_linux_distribution)
+
+case "$linux_distribution" in
+    "ubuntu" | "debian")
+        sudo apt-get update
+        sudo apt-get install llvm
+        ;;
+    "fedora" | "centos" | "rhel")
+        sudo yum update
+        sudo yum install llvm
+        ;;
+    "opensuse" | "suse")
+        sudo zypper refresh
+        sudo zypper install llvm
+        ;;
+    *)
+        echo "The supported Linux distribution could not be determined."
+        exit 1
+        ;;
+esac
+
 if [ $# -eq 0 ]; then
     echo "Using: weepsbox TARGET"
     exit 1
